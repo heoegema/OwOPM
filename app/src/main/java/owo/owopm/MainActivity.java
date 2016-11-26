cb7450893d4d3b2be0cd3ccd12e240f1a264d031
tnychka <tyler.nychka@gmail.com>
2016-11-26 15:00:08 -0500

    :(

--- app/src/main/java/owo/owopm/MainActivity.java
+++ app/src/main/java/owo/owopm/MainActivity.java
@@ -1,15 +1,51 @@
 package owo.owopm;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.gson.Gson;
+import com.microsoft.projectoxford.vision.VisionServiceClient;
+import com.microsoft.projectoxford.vision.VisionServiceRestClient;
+import com.microsoft.projectoxford.vision.contract.AnalyzeResult;
+import com.microsoft.projectoxford.vision.contract.Category;
+import com.microsoft.projectoxford.vision.contract.Face;
+import com.microsoft.projectoxford.vision.rest.VisionServiceException;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Flag to indicate which task is to be performed.
+    private static final int REQUEST_SELECT_IMAGE = 0;
+
+    // The button to select an image
+    private Button mButtonSelectImage;
+
+    // The URI of the image selected to detect.
+    private Uri mImageUri;
+
+    // The image selected to detect.
+    private Bitmap mBitmap;
+
+    // The edit to show status and result.
+    private EditText mEditText;
+
+    private VisionServiceClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +58,75 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Admiral Snackbar....", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        if (client==null){
+            client = new VisionServiceRestClient("f871c0afcfb74f5b97d47473bec35725");
+        }
+        mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.kat);
+        doAnalyze();
+    }
+
+    public void doAnalyze() {
+
+        try {
+            new doRequest().execute();
+        } catch (Exception e)
+        {
+        }
+    }
+
+    private String process() throws VisionServiceException, IOException {
+        Gson gson = new Gson();
+        String[] features = {"ImageType", "Color", "Faces", "Adult", "Categories"};
+
+        // Put the image into an input stream for detection.
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, output);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(output.toByteArray());
+
+        AnalyzeResult v = this.client.analyzeImage(inputStream, features);
+
+        String result = gson.toJson(v);
+        Log.d("result", result);
+
+        return result;
+    }
+
+    private class doRequest extends AsyncTask<String, String, String> {
+        // Store error message
+        private Exception e = null;
+
+        public doRequest() {
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            try {
+                return process();
+            } catch (Exception e) {
+                this.e = e;    // Store error
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            super.onPostExecute(data);
+            // Display based on error existence
+
+            if (e != null) {
+                this.e = null;
+            } else {
+                Gson gson = new Gson();
+                AnalyzeResult result = gson.fromJson(data, AnalyzeResult.class);
+            }
+
+        }
     }
 
     @Override
