ac5f03b717decec768444b79a919899b3a0a711b
heoegema <hollyoegema@gmail.com>
2016-11-26 17:15:40 -0500

    Fixed things.

--- app/src/main/java/owo/owopm/ViewResults.java
+++ app/src/main/java/owo/owopm/ViewResults.java
@@ -1,13 +1,28 @@
 package owo.owopm;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
 
 public class ViewResults extends AppCompatActivity {
-
+    private VideoView mVideoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_results);
+
+        mVideoView = (VideoView) findViewById(R.id.videoView);
+        mVideoView.setVisibility(View.VISIBLE);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.elephanttrunk);
+        mVideoView.setVideoURI(uri);
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mVideoView.start();
+            }
+        });
     }
 }
