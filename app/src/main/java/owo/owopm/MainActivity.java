8b69e99edfabe3928d3c733ae058ac7517ff2f4a
heoegema <hollyoegema@gmail.com>
2016-11-26 15:02:00 -0500

    Photo taking ability, takes photo and displays on screen

--- app/src/main/java/owo/owopm/MainActivity.java
+++ app/src/main/java/owo/owopm/MainActivity.java
@@ -1,6 +1,10 @@
 package owo.owopm;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.audiofx.BassBoost;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +12,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final int ACTIVITY_START_CAMERA_APP = 0;
+    private ImageView mPhotoCapturedImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +27,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mPhotoCapturedImageView= (ImageView) findViewById(R.id.capturePhotoImageView);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -49,4 +53,40 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void takePicture(View view) {
+        /*Intent callCameraApplicationIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (callCameraApplicationIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(callCameraApplicationIntent, ACTIVITY_START_CAMERA_APP);
+        }*/
+        Toast.makeText(this, "Notices Bulge", Toast.LENGTH_SHORT).show();
+
+        Intent callCameraApplicationIntent = new Intent();
+        callCameraApplicationIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(callCameraApplicationIntent, ACTIVITY_START_CAMERA_APP);
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == ACTIVITY_START_CAMERA_APP && resultCode == RESULT_OK) {
+            //Toast.makeText(this, "OwO What's this?", Toast.LENGTH_SHORT).show();
+            Bundle extras = data.getExtras();
+            Bitmap photoCapturedBitmap = (Bitmap) extras.get("data");
+            mPhotoCapturedImageView.setImageBitmap(photoCapturedBitmap);
+
+            Toast.makeText(this, "OwO What's this?", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+
+
+
 }
